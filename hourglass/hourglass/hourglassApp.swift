f5b08c32f4bbbb48e91d67b669a951b1b915ffdc
McKay Archibald <mck.archibald@gmail.com>
2025-01-02 16:56:40 -0700

    create swift project

---
+++ hourglass/hourglass/hourglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  hourglassApp.swift
+//  hourglass
+//
+//  Created by McKay Archibald on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct hourglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
