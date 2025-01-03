8ddbf56fcd04588e985bdc41efc6a0db48393279
McKay Archibald <mck.archibald@gmail.com>
2025-01-03 16:35:23 -0700

    landmarks tutorial: complete section 4-6

---
+++ hourglass/hourglass/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  hourglass
+//
+//  Created by McKay Archibald on 1/3/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
