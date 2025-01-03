cdfd2e479f5f7cbf4d48b332196b9b8559881d48
McKay Archibald <mck.archibald@gmail.com>
2025-01-02 17:29:43 -0700

    tutorial: add basic functionality

--- hourglass/hourglass/ContentView.swift
+++ hourglass/hourglass/ContentView.swift
@@ -8,25 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+
+    @State var selected = "Baseball"
+    
     var body: some View {
         VStack {
             Text("Why not try…")
                 .font(.largeTitle.bold())
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
                 Circle()
                     .fill(.blue)
                     .padding()
                     .overlay(
-                        Image(systemName: "figure.archery")
+                        Image(systemName: "figure.\(selected.lowercased())")
                             .font(.system(size: 144))
                             .foregroundColor(.white)
                     )
-                Text("Archery!")
+                Text("\(selected)!")
                     .font(.title)
             }
+            Button("Try again") {
+                selected = activities.randomElement() ?? "Archery"
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
