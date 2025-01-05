e69da4e4ea58c3f76e35497dba744db565e8efab
McKay Archibald <mck.archibald@gmail.com>
2025-01-05 15:02:38 -0700

    landmarks tutorial: handling user input section 3-7

---
+++ hourglass/hourglass/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  hourglass
+//
+//  Created by McKay Archibald on 1/5/25.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
--- hourglass/hourglass/Views/LandmarkList.swift
+++ hourglass/hourglass/Views/LandmarkList.swift
@@ -8,23 +8,31 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    @State private var showFavoritesOnly = true
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks: [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     
     var body: some View {
         NavigationSplitView {
-            List (filteredLandmarks) {landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites only")
+                }
+                
+                ForEach (filteredLandmarks) {landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
+            .animation(.default, value: filteredLandmarks)
             .navigationTitle("Landmarks")
         } detail: {
             Text ("Select a Landmark")
@@ -34,4 +42,5 @@ struct LandmarkList: View {
 
 #Preview {
     LandmarkList()
+        .environment(ModelData())
 }
