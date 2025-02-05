e7176e0c25624a5451661f9a239bbeac0ef28ae2
McKay Archibald <mck.archibald@gmail.com>
2025-02-05 00:05:34 -0700

    landmarks tutorial: building lists and navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by McKay Archibald on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by McKay Archibald on 2/4/25.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationSplitView {
+            List(landmarks) { landmark in
+                NavigationLink{
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
